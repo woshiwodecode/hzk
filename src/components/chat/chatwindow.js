d76ed5168e57d920000d1ff491024fd84d809fcd
woshiwodecode <feelthelight_k@sina.com>
2019-04-21 16:00:42 +0800

    聊天功能完成

--- src/components/chat/chatwindow.js
+++ src/components/chat/chatwindow.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { NavBar, Icon, TextareaItem, Button } from 'antd-mobile'
+import handle, { IMEvent } from './wsclient'
 import axios from '../../utils/http'
 import './chatwindow.css'
 
@@ -29,7 +30,8 @@ class ChatWindow extends Component {
     this.state = {
       listdata: [], // 聊天内容数据
       msgContent: 'abc',
-      currUserName: '' // 显示当前用户名
+      currUserName: '', // 显示当前用户名
+      client: null
     }
   }
 
@@ -56,14 +58,69 @@ class ChatWindow extends Component {
     this.setState({
       listdata: data.list
     })
+
+    // 聊天第一步->建立连接->注册用户
+    // 不能随便写->每个ws聊天的服务端代码不一样->使用当前服务端暴露的对象|方法实现ws功能
+    const uid = localStorage.getItem('uid')
+    // client指的是注册成功的某个用户客户端a
+    const client = handle(uid, data => {
+      // data指的是服务端转发后返回的消息
+      // console.log(data)
+      this.reseMsg(data)
+    })
+    this.setState({
+      client
+    })
+  }
+  // 聊天第三步->接收后台返回的消息->把消息渲染在页面上
+  reseMsg = data => {
+    // console.log(data) // data{content:'对方不在线',type}
+    // 这里暂时不知道data的结构->下面的代码->可能有问题
+    if (data.content === '对方不在线') {
+      return
+    }
+    // 如果data是对象 data.item->{}
+
+    let listdata = [...this.state.listdata]
+    listdata.push(JSON.parse(data.content))
+    this.setState({
+      listdata
+    })
   }
   // 聊天关闭
   closeChat = () => {
-    console.log('关闭聊天')
+    // console.log('关闭聊天')
+    const { changeIsShow } = this.props
+    // console.log(changeIsShow)
+    changeIsShow(false)
   }
   // 发送消息
   sendMsg = () => {
-    console.log('发送消息')
+    const { client, msgContent } = this.state
+    const {
+      item: { from_user, to_user, avatar }
+    } = this.props
+    // 聊天第二步->发送消息->客户端发送ws的请求
+    // http post body
+    // ws  发送 消息内容
+
+    // console.log('发送消息')
+    let pdata = {
+      id: Math.random() + '',
+      from_user: from_user,
+      to_user: to_user,
+      avatar: avatar,
+      chat_msg: msgContent
+    }
+    // emit事件(事件名,消息内容)
+    client.emitEvent(IMEvent.MSG_TEXT_SEND, JSON.stringify(pdata))
+
+    // 更新视图
+    let listdata = [...this.state.listdata]
+    listdata.push(pdata)
+    this.setState({
+      listdata
+    })
   }
   // 受控组件
   handleMsgChange = v => {
